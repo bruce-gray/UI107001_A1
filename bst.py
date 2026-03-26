467607831357eda8c9dbf520c7b7cb835a6d4993
bruce-gray <brucefledge@gmail.com>
2026-03-26 19:17:49 +0000

    added BST search method

--- bst.py
+++ bst.py
@@ -22,3 +22,22 @@ class BST:
         elif data > node.data:
             node.right = self._insert_recursive(node.right,data)
         return node
+    
+    def search(self,data):
+        return self._search_recursive(self.root,data)
+
+    def _search_recursive(self,node,data):
+        # base case - tree is empty or value is not in tree returns False
+        if node is None:
+            return False
+
+        # base case - if value found returns True
+        if node.data == data:
+            return True
+
+        # recursive cases - eliminate essentially half of the tree at each step of the search O(log n)
+        if node.data > data:
+            return self._search_recursive(node.left,data)
+        elif node.data < data:
+            return self._search_recursive(node.right,data)
+        return False
