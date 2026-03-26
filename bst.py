42d8ae7de4486b0251e8b70a97a068e7cbf3879d
bruce-gray <brucefledge@gmail.com>
2026-03-26 17:56:41 +0000

    fix indentation

--- bst.py
+++ bst.py
@@ -8,17 +8,17 @@ class BST:
     def __init__(self):
         self.root = None # empty tree has no starting node
 
-def insert(self,data):
-    self.root = self._insert_recursive(self.root,data)
+    def insert(self,data):
+        self.root = self._insert_recursive(self.root,data)
 
-def _insert_recursive(self,node,data):
-    # base case - when empty spot found, place the new node here
-    if node is None:
-        return Node(data)
-    
-    # recursive cases - traverses left or right depending on the value
-    if data < node.data:
-        node.left = self._insert_recursive(node.left,data)
-    elif data > node.data:
-        node.right = self._insert_recursive(node.right,data)
-    return node
+    def _insert_recursive(self,node,data):
+        # base case - when empty spot found, place the new node here
+        if node is None:
+            return Node(data)
+        
+        # recursive cases - traverses left or right depending on the value
+        if data < node.data:
+            node.left = self._insert_recursive(node.left,data)
+        elif data > node.data:
+            node.right = self._insert_recursive(node.right,data)
+        return node
